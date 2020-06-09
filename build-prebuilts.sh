35024c25e71df94aedebacd5c4e1dc13f5b57d9d
Matthias Maennich <maennich@google.com>
2020-06-09 22:54:00 +0200

    Kernel build tools: initial prebuilt script
    Add the initial build-prebuilts script to initiate the soong build and
    to package up the prebuilt binaries.
    
    Bug: 150391496
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Change-Id: I4f03308b746ee1f4472c44bc916b86bf43352621

--- build-prebuilts.sh
+++ build-prebuilts.sh
@@ -1,5 +1,53 @@
 #!/bin/bash -ex
 
-env
+OUT_DIR=${OUT_DIR:-out}
+TOP=$(pwd)
+OS=linux
+
+build_soong=1
+clean=t
+[[ "${1:-}" != '--resume' ]] || clean=''
+
+# Use toybox and other prebuilts even outside of the build (test running, go, etc)
+export PATH=${TOP}/prebuilts/build-tools/path/${OS}-x86:$PATH
+
+if [ -n ${build_soong} ]; then
+    SOONG_OUT=${OUT_DIR}/soong
+    SOONG_HOST_OUT=${OUT_DIR}/soong/host/${OS}-x86
+    [[ -z "${clean}" ]] || rm -rf ${SOONG_OUT}
+    mkdir -p ${SOONG_OUT}
+    cat > ${SOONG_OUT}/soong.variables << EOF
+{
+    "Allow_missing_dependencies": true,
+    "HostArch":"x86_64"
+}
+EOF
+    SOONG_BINARIES=(
+        lz4
+    )
+
+    binaries="${SOONG_BINARIES[@]/#/${SOONG_HOST_OUT}/bin/}"
+
+    # Build everything
+    build/soong/soong_ui.bash --make-mode --skip-make ${binaries}
+
+    # Stage binaries
+    mkdir -p ${SOONG_OUT}/dist/bin
+    cp ${binaries} ${SOONG_OUT}/dist/bin/
+
+    # Package prebuilts
+    (
+        cd ${SOONG_OUT}/dist
+        zip -qryX build-prebuilts.zip *
+    )
+fi
+
+if [ -n "${DIST_DIR}" ]; then
+    mkdir -p ${DIST_DIR} || true
+
+    if [ -n ${build_soong} ]; then
+        cp ${SOONG_OUT}/dist/build-prebuilts.zip ${DIST_DIR}/
+    fi
+fi
 
 exit 0
